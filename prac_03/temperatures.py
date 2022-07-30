a6576a168ac93ac281d30626e4be0db23afbeadf
Joseph Tan <joseph.tan1@my.jcu.edu.au>
2022-07-30 14:14:50 +0700

    Add fahrenheit to celsius converter function

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -15,11 +15,11 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5/9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
@@ -32,3 +32,7 @@ def convert_celsius_to_fahrenheit(celsius):
     """Converts celsius to fahrenheit"""
     return celsius * 9.0 / 5 + 32
 
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Converts fahrenheit to celsius"""
+    return 5/9 * (fahrenheit - 32)
