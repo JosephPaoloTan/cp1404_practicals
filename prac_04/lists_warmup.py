26c734871812fb92739a7045547b881deee5ef7e
Joseph Tan <joseph.tan1@my.jcu.edu.au>
2022-08-09 20:01:04 +0800

    Add answers to warmup questions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -10,4 +10,14 @@ numbers[:-1] [3, 1, 4, 1, 5, 9, 2]
 numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
 
+"""1. Change the first element of numbers to "ten"""
+numbers[0] = "ten"
 
+"""2. Change the last element of numbers to 1"""
+numbers[-1] = 1
+
+"""3. Get all the elements from numbers except the first two (slice)"""
+print(numbers[2:])
+
+"""4. Check if 9 is an element of numbers"""
+9 in numbers
