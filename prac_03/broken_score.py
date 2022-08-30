93133d235faf874b6ac053aa61152afe2bb5709c
Joseph Tan <joseph.tan1@my.jcu.edu.au>
2022-08-30 23:16:53 +0800

    Revert "Add function to determine score status"
    This reverts commit 60267296f4a4a38818c6bcc287b1ee08c746c592.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,25 +1,19 @@
 """
 CP1404/CP5632 - Practical
-Program to determine score status
+Broken program to determine score status
 """
 
 # TODO: Fix this!
 
+score = float(input("Enter score: "))
 
-def main():
-    """Score status program"""
-    score = float(input("Enter score: "))
-
-
-def determine_score_status(score):
-    """Determine the status of the score"""
-    if score > 100:
-        print("Invalid score")
-    elif score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    elif score < 50:
-        print("Bad")
-    else:
-        print("Invalid score")
+if score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+elif score < 50:
+    print("Bad")
+else:
+    print("Invalid score")
